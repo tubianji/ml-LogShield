4505360a2f711d8cc62c28dba56e98cf93d5cab6
tubianji <130753460+tubianji@users.noreply.github.com>
2022-08-07 17:06:11 +0000

    Rev

--- api/dashboard.js
+++ api/dashboard.js
@@ -1,3 +1,22 @@
 const { CurrentlyBlockedUsers, TotalBlocked, TotalRpm } = require('../middleware/rateLimiter').rateLimitData;
 const { totalwafblocked } = require('../middleware/wafRules').wafData;
-const { AVGServerPing
+const { AVGServerPing } = require('../middleware/ping');
+const { bandwidth } = require('../middleware/bandwidth');
+const os = require('node:os');
+const pidusage = require('pidusage');
+
+module.exports = (router, client, checkAuth) => {
+    router.post("/admin", checkAuth, async (req, res) => {
+        const processStats = await pidusage(process.pid);
+
+        const data = {
+            "waf": {
+                "totalwafblocked": totalwafblocked()
+            },
+            "rateLimit": {
+                "CurrentlyBlockedUsers": CurrentlyBlockedUsers(),
+                "Totalblocked": Object.fromEntries(TotalBlocked().entries()),
+            },
+            "ping": {
+                "AVGServerPing": `${await AVGServerPing()}ms`,
+         
