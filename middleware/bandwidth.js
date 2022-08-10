a35e5b59d29acc4b9690ccb19f4143718d8d2049
tubianji <130753460+tubianji@users.noreply.github.com>
2022-08-10 07:13:56 +0000

    Auto update dev build [publish.sh]

--- middleware/bandwidth.js
+++ middleware/bandwidth.js
@@ -10,4 +10,25 @@ function formatBytes(bytes, decimals = 2) {
 
   const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ['B', 'KB', 'MB', 'GB
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+const bandwidth = {
+  totalUpload: () => formatBytes(totalUpload),
+  totalDownload: () => formatBytes(totalDownload),
+  avgUploadSize: () => formatBytes(totalUpload / totalRequests),
+  avgDownloadSize: () => formatBytes(totalDownload / totalRequests),
+};
+
+router.use((req, res, next) => {
+  const requestBodySize = JSON.stringify(req.body).length;
+  totalUpload += requestBodySize;
+  totalRequests++;
+
+  const originalWrite = res.write;
+  const originalEnd = res.end;
+  l
