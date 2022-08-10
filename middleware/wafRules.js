6516bca9cfb6c9b279e7aaf6ad8d0a0c3cf91150
tubianji <130753460+tubianji@users.noreply.github.com>
2022-08-10 16:14:18 +0000

    Add missing translations.

--- middleware/wafRules.js
+++ middleware/wafRules.js
@@ -20,4 +20,24 @@ function checkAgainstWAFRules(value) {
 }
 
 router.use((req, res, next) => {
-  const whitelisted = (process.env.WHITELISTED || '').split(',').ma
+  const whitelisted = (process.env.WHITELISTED || '').split(',').map(ip => ip.trim()).filter(ip => ip.length > 0);
+  if (whitelisted.includes(req.ip)) {
+    return next();
+  }
+  if (req.session && req.session.whitelisted) {
+    return next();
+  }
+  const queryParams = Object.values(req.query).join(' ');
+  const bodyParams = Object.values(req.body).join(' ');
+
+  if (checkAgainstWAFRules(queryParams) || checkAgainstWAFRules(bodyParams)) {
+    return res.status(403).send('Request blocked by WAF');
+  }
+
+  next();
+});
+
+const totalwafblocked = () => totalblocked;
+
+module.exports = router;
+module.exports.wafData = { totalwafblocked };
