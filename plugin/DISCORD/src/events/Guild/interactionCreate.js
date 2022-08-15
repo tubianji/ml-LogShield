7da62ad35e8007cdf6311d3c39f9513a6091aaf2
tubianji <130753460+tubianji@users.noreply.github.com>
2022-08-15 18:06:10 +0000

    Recompile

--- plugin/DISCORD/src/events/Guild/interactionCreate.js
+++ plugin/DISCORD/src/events/Guild/interactionCreate.js
@@ -24,4 +24,32 @@ client.on("interactionCreate", async (interaction) => {
           PermissionsBitField.resolve(permission)
         )
       ) {
-        missingperms.push(
+        missingperms.push(permission);
+      } else {
+        return;
+      }
+    });
+
+    if (missingperms.length > 0) {
+      const embed = new EmbedBuilder()
+        .setDescription(
+          `🚫 Unfortunately, I can't run this command. I need the following permissions: \n \`${missingperms.join(
+            ", "
+          )}\``
+        )
+        .setColor("Red");
+      return interaction.reply({ embeds: [embed] });
+    }
+  }
+
+  if (command.permissions) {
+    if (
+      !interaction.member.permissions.has(
+        PermissionsBitField.resolve(command.permissions || [])
+      )
+    )
+      return interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setDescription(
+              `🚫 Unfortunat
