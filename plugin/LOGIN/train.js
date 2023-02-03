7ef00c7fa4c3bfb8186baa7d6522b08a78500f57
tubianji <130753460+tubianji@users.noreply.github.com>
2023-02-03 20:51:13 +0000

    Updated changes.txt

--- plugin/LOGIN/train.js
+++ plugin/LOGIN/train.js
@@ -6,4 +6,34 @@ function tokenizeText(text) {
 }
 
 function preprocessText(text) {
- 
+  const lowercasedText = text.toLowerCase();
+
+  return lowercasedText;
+}
+
+function createModel(vocabSize, inputDim) {
+  const model = tf.sequential();
+  model.add(
+    tf.layers.embedding({
+      inputDim: vocabSize,
+      outputDim: inputDim,
+      inputLength: null,
+    })
+  );
+  model.add(
+    tf.layers.bidirectional({
+      layer: tf.layers.simpleRNN({ units: 16, returnSequences: true }),
+      mergeMode: 'concat',
+    }),
+  );
+  model.add(
+    tf.layers.bidirectional({
+      layer: tf.layers.simpleRNN({ units: 16 }),
+      mergeMode: 'concat',
+    }),
+  );
+  model.add(tf.layers.dense({ units: 1, activation: "sigmoid" }));
+
+  model.compile({
+    optimizer: "adam",
+    loss: "binaryCrossentropy"
