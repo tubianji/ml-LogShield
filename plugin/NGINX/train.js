3367cc59ddc1f05202c481d75edc6fa13e63a70a
tubianji <130753460+tubianji@users.noreply.github.com>
2023-05-01 11:28:11 +0000

    goo

--- plugin/NGINX/train.js
+++ plugin/NGINX/train.js
@@ -19,4 +19,28 @@ const maliciousLogDataArray = readLogs('./plugin/NGINX/Train/malicious');
 
 function parseNginxLogs(logData) {
     const logs = logData.split('\n');
-    const logRegex = /^(\S+) 
+    const logRegex = /^(\S+) - - \[(.+)\] "(\S+) (.+?) (\S+)" (\d+) ?(\d+)? "([^"]*)" "([^"]*)"/;
+  
+    const parsedLogs = logs.map((log) => {
+      const match = log.match(logRegex);
+  
+      if (match) {
+        return {
+          ip: match[1],
+          timestamp: match[2],
+          method: match[3],
+          uri: match[4],
+          protocol: match[5],
+          statusCode: parseInt(match[6], 10),
+          bytesSent: match[7] ? parseInt(match[7], 10) : null,
+          referer: match[8],
+          userAgent: match[9],
+          label: 0,
+        };
+      }
+    });
+    return parsedLogs.filter((log) => log !== undefined);
+  }  
+
+const parsedLegitimateLogsArray = accessLogDataArray.map(logData => parseNginxLogs(logData));
+const pa
