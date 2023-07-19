32b1c7fad30d46a219352092abbff60864545aea
tubianji <130753460+tubianji@users.noreply.github.com>
2023-07-19 08:39:21 +0000

    Add back button

---
+++ website/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import Contact from "@/components/Contact";
+
+const ContactPage = () => {
+  return (
+    <>
+      <Breadcrumb
+        pageName="Contact Page"
+        description="Lorem ipsum dolor sit amet, c
