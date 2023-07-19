8824b370825ab0f8176c01a5b8149bf740a5265b
tubianji <130753460+tubianji@users.noreply.github.com>
2023-07-19 13:20:03 +0000

    update gemfile.lock

--- website/app/contact/page.tsx
+++ website/app/contact/page.tsx
@@ -6,4 +6,12 @@ const ContactPage = () => {
     <>
       <Breadcrumb
         pageName="Contact Page"
-        description="Lorem ipsum dolor sit amet, c
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+      />
+
+      <Contact />
+    </>
+  );
+};
+
+export default ContactPage;
