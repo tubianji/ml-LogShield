bf93864a31fab5abc002c841dc1f547515e64b47
tubianji <130753460+tubianji@users.noreply.github.com>
2023-07-21 19:19:19 +0000

    Update pod

--- website/app/signin/page.tsx
+++ website/app/signin/page.tsx
@@ -3,4 +3,15 @@ import Link from "next/link";
 const SigninPage = () => {
   return (
     <>
-      <section cl
+      <section className="relative z-10 overflow-hidden pt-36 pb-16 md:pb-20 lg:pt-[180px] lg:pb-28">
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4">
+              <div className="mx-auto max-w-[500px] rounded-md bg-primary bg-opacity-5 py-10 px-6 dark:bg-dark sm:p-[60px]">
+                <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
+                  Sign in to your account
+                </h3>
+                <p className="mb-11 text-center text-base font-medium text-body-color">
+                  Login to your account for a faster checkout.
+                </p>
+               
