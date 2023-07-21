44516746f967f893d7fdf11af63d9ee07786d4db
tubianji <130753460+tubianji@users.noreply.github.com>
2023-07-21 08:39:47 +0000

    github oauth

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -6,3 +6,21 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import Testimonials from "@/components/Testimonials";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+      <Features />
+      <Brands />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Testimonials />
+      <Pricing />
+    </>
+  );
+}
