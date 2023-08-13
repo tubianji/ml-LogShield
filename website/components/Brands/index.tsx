af56a9bdd0d71cb130cc2dbea2215659f9708ccf
tubianji <130753460+tubianji@users.noreply.github.com>
2023-08-13 09:59:02 +0000

    HashMap

--- website/components/Brands/index.tsx
+++ website/components/Brands/index.tsx
@@ -13,4 +13,32 @@ const brandsData: Brand[] = [
     name: "Tailgrids",
     href: "https://tailgrids.com",
     image: "/images/brands/tailgrids.svg",
- 
+  },
+  {
+    id: 3,
+    name: "Lineicons",
+    href: "https://lineicons.com",
+    image: "/images/brands/lineicons.svg",
+  },
+  {
+    id: 4,
+    name: "GrayGrids",
+    href: "https://graygrids.com",
+    image: "/images/brands/graygrids.svg",
+  },
+  {
+    id: 5,
+    name: "TailAdmin",
+    href: "https://tailadmin.com",
+    image: "/images/brands/tailadmin.svg",
+  },
+];
+
+const Brands = () => {
+  return (
+    <section className="pt-16">
+      <div className="container">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4">
+            <div
+              className="wow fadeInUp flex flex-wrap items-center justify-center rounded-md bg-dar
