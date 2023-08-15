fcb475c6ff6c691b4fb2efbb2dc10de8d46e2743
tubianji <130753460+tubianji@users.noreply.github.com>
2023-08-15 14:50:02 +0000

    Delete NAMESPACE

--- website/components/Brands/index.tsx
+++ website/components/Brands/index.tsx
@@ -41,4 +41,25 @@ const Brands = () => {
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
             <div
-              className="wow fadeInUp flex flex-wrap items-center justify-center rounded-md bg-dar
+              className="wow fadeInUp flex flex-wrap items-center justify-center rounded-md bg-dark py-8 px-8 dark:bg-primary dark:bg-opacity-5 sm:px-10 md:py-[40px] md:px-[50px] xl:p-[50px] 2xl:py-[60px] 2xl:px-[70px]"
+              data-wow-delay=".1s
+              "
+            >
+              {brandsData.map((brand) => (
+                <SingleBrand key={brand.id} brand={brand} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Brands;
+
+const SingleBrand = ({ brand }: { brand: Brand }) => {
+  const { href, image, name } = brand;
+
+  return (
+    <div className="mx-3 flex w-full max-w-[160px] items-center justify-center py-[15px] sm:mx-4 lg:max-w-[130px] xl:mx-6 xl:max-w-[150px] 2xl:mx-8 2xl:max-w-[160
