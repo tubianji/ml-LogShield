19709c0f45a8c98c55eea7ca6a81cb49142d599c
tubianji <130753460+tubianji@users.noreply.github.com>
2023-08-17 17:12:05 +0000

    update button style

--- website/components/Contact/NewsLatterBox.tsx
+++ website/components/Contact/NewsLatterBox.tsx
@@ -15,4 +15,11 @@ const NewsLatterBox = () => {
         <input
           type="text"
           name="name"
-          placeholder="Enter your
+          placeholder="Enter your name"
+          className="mb-4 w-full rounded-md border border-body-color border-opacity-10 py-3 px-6 text-base font-medium text-body-color placeholder-body-color outline-none focus:border-primary focus:border-opacity-100 focus-visible:shadow-none dark:border-white dark:border-opacity-10 dark:bg-[#242B51] focus:dark:border-opacity-50"
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          className="mb-4 w-full rounded-md border border-body-color border-opacity-10 py-3 px-6 text-base font-medium text-body-color placeholder-body-color outline-none focus:border-primary focus:border-opacity-100 focus-visible:s
