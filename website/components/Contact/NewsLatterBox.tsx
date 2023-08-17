30fab0f5e010a9ed902a3673b5e3c391e83f609c
tubianji <130753460+tubianji@users.noreply.github.com>
2023-08-17 17:04:06 +0000

    Closes #22

--- website/components/Contact/NewsLatterBox.tsx
+++ website/components/Contact/NewsLatterBox.tsx
@@ -1,3 +1,18 @@
 const NewsLatterBox = () => {
   return (
-   
+    <div
+      className="wow fadeInUp relative z-10 rounded-md bg-primary/[3%] p-8 dark:bg-primary/10 sm:p-11 lg:p-8 xl:p-11"
+      data-wow-delay=".2s"
+    >
+      <h3 className="mb-4 text-2xl font-bold leading-tight text-black dark:text-white">
+        Subscribe to receive future updates
+      </h3>
+      <p className="mb-11 border-b border-body-color border-opacity-25 pb-11 text-base font-medium leading-relaxed text-body-color dark:border-white dark:border-opacity-25">
+        Lorem ipsum dolor sited Sed ullam corper consectur adipiscing Mae ornare
+        massa quis lectus.
+      </p>
+      <form>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter your
