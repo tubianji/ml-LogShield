99303dd5365912b178feb92aa0bcc8ea716a13a0
tubianji <130753460+tubianji@users.noreply.github.com>
2023-08-23 10:13:58 +0000

    lesson 3

--- website/components/Contact/NewsLatterBox.tsx
+++ website/components/Contact/NewsLatterBox.tsx
@@ -22,4 +22,17 @@ const NewsLatterBox = () => {
           type="email"
           name="email"
           placeholder="Enter your email"
-          className="mb-4 w-full rounded-md border border-body-color border-opacity-10 py-3 px-6 text-base font-medium text-body-color placeholder-body-color outline-none focus:border-primary focus:border-opacity-100 focus-visible:s
+          className="mb-4 w-full rounded-md border border-body-color border-opacity-10 py-3 px-6 text-base font-medium text-body-color placeholder-body-color outline-none focus:border-primary focus:border-opacity-100 focus-visible:shadow-none dark:border-white dark:border-opacity-10 dark:bg-[#242B51] focus:dark:border-opacity-50"
+        />
+        <input
+          type="submit"
+          value="Subscribe"
+          className="duration-80 mb-4 w-full cursor-pointer rounded-md border border-transparent bg-primary py-3 px-6 text-center text-base font-medium text-white outline-none transition ease-in-out hover:bg-opacity-80 hover:shadow-signUp focus-visible:shadow-none"
+        />
+        <p className="text-center text-base font-medium leading-relaxed text-body-color">
+          No spam guaranteed, So please don’t send any spam mail.
+        </p>
+      </form>
+      <div className="absolute top-0 left-0 z-[-1]">
+        <svg
+      
