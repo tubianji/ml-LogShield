1d4f99614c9a850c3b470f75b73c4d74d301dd7d
tubianji <130753460+tubianji@users.noreply.github.com>
2023-10-08 21:25:41 +0000

    composer update.

--- website/components/Features/index.tsx
+++ website/components/Features/index.tsx
@@ -11,4 +11,20 @@ const Features = () => {
       >
         <div className="container">
           <SectionTitle
-            title=
+            title="Main Features"
+            paragraph="LogShield is a comprehensive machine learning project that leverages the powerful TensorFlow.js to monitor and detect malicious activity in both web server access and login logs. This robust system offers a safeguard for your server, detecting and flagging suspicious behaviors to help protect against potential security breaches."
+            center
+          />
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
+            {featuresData.map((feature) => (
+              <SingleFeature key={feature.id} feature={feature} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Features;
---
+++ website/components/Footer/index.tsx
@@ -0,0 +1,19 @@
+
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <>
+      <footer
+        className="wow fadeInUp relative z-10 bg-primary bg-opacity-5 pt-16 md:pt-20 lg:pt-24"
+        data-wow-delay=".1s"
+      >
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-5/12">
+              <div className="mb-12 max-w-[360px] lg:mb-16">
+                <Link href="/" className="mb-8 inline-block">
+                  <Image
+                    src="/logo.png"
+                    alt="logo"
