ad8c2d270ccb2c98402368f9b6be43b9b11086a2
tubianji <130753460+tubianji@users.noreply.github.com>
2023-10-08 11:25:10 +0000

    Kick from realglobe-Inc/sugo-actor

---
+++ website/components/Features/index.tsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleFeature from "./SingleFeature";
+import featuresData from "./featuresData";
+
+const Features = () => {
+  return (
+    <>
+      <section
+        id="features"
+        className="bg-primary/[.03] py-16 md:py-20 lg:py-28"
+      >
+        <div className="container">
+          <SectionTitle
+            title=
