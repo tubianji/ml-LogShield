4346ce9a3decb893efcbdcea9a2bc3995a87d1a8
tubianji <130753460+tubianji@users.noreply.github.com>
2023-10-11 10:41:25 +0000

    af_unix: speedup /proc/net/unix

---
+++ website/components/Header/menuData.tsx
@@ -0,0 +1,14 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Docs",
+    path: "/Docs",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Support",
+    path: "/contact",
+   
