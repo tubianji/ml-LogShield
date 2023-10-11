e9cfcd06ad861f0b0c85ed59ee73c949394920ac
tubianji <130753460+tubianji@users.noreply.github.com>
2023-10-11 16:06:42 +0000

    Simplify error handling

--- website/components/Hero/index.tsx
+++ website/components/Hero/index.tsx
@@ -14,4 +14,12 @@ const Hero = () => {
                 className="wow fadeInUp mx-auto max-w-[800px] text-center"
                 data-wow-delay=".2s"
               >
-                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leadin
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                LogShield your one stop open-source privacy shield!
+                </h1>
+                <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
+                LogShield is a comprehensive machine learning project that leverages the powerful TensorFlow.js to monitor and detect malicious activity in both web server access and login logs.
+                </p>
+                <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                  <Link
+                    href="https://github.com/Frost-Lord/LogShield/pkgs/container/logshield"
