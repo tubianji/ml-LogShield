ac00a669a618f58c183c0cb4d08f5ba1d65c602c
tubianji <130753460+tubianji@users.noreply.github.com>
2023-11-09 12:51:16 +0000

    v7.0.1

--- website/components/Pricing/PricingBox.tsx
+++ website/components/Pricing/PricingBox.tsx
@@ -18,4 +18,18 @@ const PricingBox = (props: {
             $<span className="amount">{price}</span>
             <span className="time text-body-color">/{duration}</span>
           </h3>
-          <h4 classNam
+          <h4 className="mb-2 text-xl font-bold text-dark dark:text-white">
+            {packageName}
+          </h4>
+        </div>
+        <p className="mb-7 text-base text-body-color">{subtitle}</p>
+        <div className="mb-8 border-b border-body-color border-opacity-10 pb-8 dark:border-white dark:border-opacity-10">
+          <button className="flex w-full items-center justify-center rounded-md bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
+            Start Free Trial
+          </button>
+        </div>
+        <div>{children}</div>
+        <div className="absolute bottom-0 right-0 z-[-1]">
+          <svg
+            width="179"
+         
