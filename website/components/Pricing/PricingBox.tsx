b0906473a9d5159a8db57ccd5e89986f88d52d69
tubianji <130753460+tubianji@users.noreply.github.com>
2023-11-09 05:28:53 +0000

    More refactoring [skip hazardlib]

--- website/components/Pricing/PricingBox.tsx
+++ website/components/Pricing/PricingBox.tsx
@@ -1,3 +1,21 @@
 const PricingBox = (props: {
   price: string;
-  dur
+  duration: string;
+  packageName: string;
+  subtitle: string;
+  children: React.ReactNode;
+}) => {
+  const { price, duration, packageName, subtitle, children } = props;
+
+  return (
+    <div className="w-full">
+      <div
+        className="wow fadeInUp relative z-10 rounded-md bg-white px-8 py-10 shadow-signUp dark:bg-[#1D2144]"
+        data-wow-delay=".1s"
+      >
+        <div className="flex items-center justify-between">
+          <h3 className="price mb-2 text-3xl font-bold text-black dark:text-white">
+            $<span className="amount">{price}</span>
+            <span className="time text-body-color">/{duration}</span>
+          </h3>
+          <h4 classNam
