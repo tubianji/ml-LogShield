bda28fa78c503e8069b02c17186d7aa8cf92adbf
tubianji <130753460+tubianji@users.noreply.github.com>
2023-11-19 08:19:01 +0000

    Minor code improvements

--- website/components/Pricing/index.tsx
+++ website/components/Pricing/index.tsx
@@ -12,4 +12,25 @@ const Pricing = () => {
       <div className="container">
         <SectionTitle
           title="Simple and Affordable Pricing"
-          pa
+          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+          center
+          width="665px"
+        />
+
+        <div className="w-full">
+          <div
+            className="wow fadeInUp mb-8 flex justify-center md:mb-12 lg:mb-16"
+            data-wow-delay=".1s"
+          >
+            <span
+              onClick={() => setIsMonthly(true)}
+              className={`${
+                isMonthly
+                  ? "pointer-events-none text-primary"
+                  : "text-dark dark:text-white"
+              } mr-4 cursor-pointer text-base font-semibold`}
+            >
+              Monthly
+            </span>
+            <div
+              onClick={() => 
