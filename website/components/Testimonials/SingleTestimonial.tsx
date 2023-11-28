6e2305a26a6aa7bd058a60ba4051c2676e7b1352
tubianji <130753460+tubianji@users.noreply.github.com>
2023-11-28 06:19:11 +0000

    fix: remove frontmatter from provider readme

--- website/components/Testimonials/SingleTestimonial.tsx
+++ website/components/Testimonials/SingleTestimonial.tsx
@@ -2,4 +2,27 @@ import { Testimonial } from "@/types/testimonial";
 import Image from "next/image";
 const starIcon = (
   <svg width="18" height="16" viewBox="0 0 18 16" className="fill-current">
-    <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149
+    <path d="M9.09815 0.361679L11.1054 6.06601H17.601L12.3459 9.59149L14.3532 15.2958L9.09815 11.7703L3.84309 15.2958L5.85035 9.59149L0.595291 6.06601H7.0909L9.09815 0.361679Z" />
+  </svg>
+);
+
+const SingleTestimonial = ({ testimonial }: { testimonial: Testimonial }) => {
+  const { star, name, image, content, designation } = testimonial;
+
+  let ratingIcons = [];
+  for (let index = 0; index < star; index++) {
+    ratingIcons.push(
+      <span key={index} className="text-yellow">
+        {starIcon}
+      </span>
+    );
+  }
+
+  return (
+    <div className="w-full">
+      <div
+        className="wow fadeInUp rounded-md bg-white p-8 shadow-one dark:bg-[#1D2144] lg:px-5 xl:px-8"
+        data-wow-delay=".1s"
+      >
+        <div className="mb-5 flex items-center space-x-1">{ratingIcons}</div>
+     
