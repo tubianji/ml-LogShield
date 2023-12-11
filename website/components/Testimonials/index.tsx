669420ce0d587635f21981fce6f21bf41c728d2d
tubianji <130753460+tubianji@users.noreply.github.com>
2023-12-11 11:47:34 +0000

    drm: Prevent overwriting from userspace underallocating core ioctl structs

--- website/components/Testimonials/index.tsx
+++ website/components/Testimonials/index.tsx
@@ -146,4 +146,24 @@ const Testimonials = () => {
               y1="42.6717"
               x2="-40.8688"
               y2="-2.84285"
-      
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#4A6CF7" stopOpacity="0" />
+              <stop offset="1" stopColor="#4A6CF7" />
+            </linearGradient>
+            <linearGradient
+              id="paint2_linear_72:302"
+              x1="256.267"
+              y1="64.6717"
+              x2="-40.8688"
+              y2="19.1572"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#4A6CF7" stopOpacity="0" />
+              <stop offset="1" stopColor="#4A6CF7" />
+            </linearGradient>
+            <linearGradient
+              id="paint3_linear_72:302"
+              x1="256.267"
+              y1="76.6717"
+          
