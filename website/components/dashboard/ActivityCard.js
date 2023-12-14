c6bb0953a3f68c6f3793f32069c2710203cb38b6
tubianji <130753460+tubianji@users.noreply.github.com>
2023-12-14 09:16:10 +0000

    J

--- website/components/dashboard/ActivityCard.js
+++ website/components/dashboard/ActivityCard.js
@@ -5,4 +5,23 @@ const ActivityCard = () => {
     const actions = [
         { user: '127.0.0.1', date: '2023-05-12', action: 'Banned', response: 'Success' },
         { user: '127.0.0.1', date: '2023-05-12', action: 'Banned', response: 'Failed' },
-        { use
+        { user: '127.0.0.1', date: '2023-05-12', action: 'Banned', response: 'Success' }
+    ];
+
+    return (
+        <Card title="Recent Actions:">
+            <br></br>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th>Source IP:</th>
+                        <th>Date</th>
+                        <th>Action</th>
+                        <th>Response</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <br></br>
+                    {actions.map((action, index) => (
+                        <tr key={index}>
+                            <td>{a
