b3b8c921fe2bf04b0979e0a97c5bbe2378c53aa3
tubianji <130753460+tubianji@users.noreply.github.com>
2023-12-16 20:54:27 +0000

    Typo in comment

--- website/components/dashboard/DashboardArea.js
+++ website/components/dashboard/DashboardArea.js
@@ -9,4 +9,22 @@ import NodeCard from './NodeCard';
 import LargestAttacks from './LargestAttacks';
 import APICard from './APICard';
 import LoadingBar from './LoadingBar';
-import Performance from '.
+import Performance from './Performance';
+
+const DashboardArea = () => {
+    const [loading, setLoading] = React.useState(true);
+    const [data, setData] = React.useState([]);
+
+    React.useEffect(() => {
+        const nodes = process.env.NEXT_PUBLIC_NODES.split(',').map(node => node.trim());
+        
+        const fetchData = async () => {
+            const results = await Promise.all(nodes.map(node =>
+                axios.post(`${node}/logshield/api/admin`, {
+                    "auth": "aielgv8sgeasgryleairgearihu",
+                }).catch(err => console.log(err))
+            ));
+
+            const data = results.map(result => result.data);
+            setData(data);
+           
