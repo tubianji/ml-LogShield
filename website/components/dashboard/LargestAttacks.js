fb482c5ff2067bcfe9ed58b838474c83e8feeac5
tubianji <130753460+tubianji@users.noreply.github.com>
2023-12-18 10:10:04 +0000

    removed garbage

--- website/components/dashboard/LargestAttacks.js
+++ website/components/dashboard/LargestAttacks.js
@@ -10,4 +10,34 @@ const mergeTotalblockedData = (data) => {
   }
   const mergedData = data.reduce((acc, nodeData) => {
     if (nodeData.rateLimit && nodeData.rateLimit.Totalblocked) {
-      for (let [key, value] of Object.e
+      for (let [key, value] of Object.entries(nodeData.rateLimit.Totalblocked)) {
+        if (!acc[key]) {
+          acc[key] = value;
+        } else {
+          acc[key] += value;
+        }
+      }
+    }
+    acc["2023-05-14"] = 1 //remove this later
+    return acc;
+  }, {});
+
+  return mergedData;
+}
+
+const LargestAttacks = ({ data }) => {
+  const chartRef = React.useRef(null);
+  const chartInstanceRef = React.useRef(null);
+  
+  const mergedData = mergeTotalblockedData(data);
+
+  const chartData = {
+    labels: Object.keys(mergedData),
+    datasets: [
+      {
+        label: 'Amount',
+        data: Object.values(mergedData),
+        fill: false,
+        backgroundColor: 'transparent',
+        borderColor: '#1c80d8',
+        pointBackgroundCo
