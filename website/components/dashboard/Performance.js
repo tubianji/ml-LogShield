9d59a58a7d8a18d3d6e2488479caabc3c5e50a37
tubianji <130753460+tubianji@users.noreply.github.com>
2023-12-24 08:38:57 +0000

    lab1

--- website/components/dashboard/Performance.js
+++ website/components/dashboard/Performance.js
@@ -14,4 +14,42 @@ const Performance = () => {
           label: 'Requests',
           data: [50, 100, 150, 180, 90, 120, 200],
           fill: false,
-          backgroundColor: 'transparent'
+          backgroundColor: 'transparent',
+          borderColor: '#1c80d8',
+          pointBackgroundColor: '#1c80d8',
+          borderWidth: 2,
+          tension: 0.4,
+        },
+      ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+      x: {
+        type: 'category',
+      },
+    },
+    elements: {
+      point: {
+        radius: 4,
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    maintainAspectRatio: false,
+  };
+
+  React.useEffect(() => {
+    if (chartRef.current) {
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy();
+      }
+      chartInstanceRef.current = new Chart(chartRef.current, {
+        type: 'line',
+        data: data
