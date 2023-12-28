5d713e504d3c5f19e05f8df37191d737fdefe1c2
tubianji <130753460+tubianji@users.noreply.github.com>
2023-12-28 09:25:15 +0000

    add mobile

--- website/components/dashboard/Sidebar.js
+++ website/components/dashboard/Sidebar.js
@@ -7,4 +7,21 @@ const Sidebar = () => {
     return (
         <div className={styles.sidebar}>
             <nav>
-                <div className={styles.acti
+                <div className={styles.active}>
+                    <FontAwesomeIcon icon={faHome} className={styles.icon} />
+                     Dashboard
+                </div>
+                <div className={styles['nav-item']}>
+                    <FontAwesomeIcon icon={faCog} className={styles.icon} />
+                     API
+                </div>
+                <div className={styles['nav-item']}>
+                    <FontAwesomeIcon icon={faTools} className={styles.icon} />
+                     Settings
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Sidebar;
---
+++ website/components/dashboard/TopNavBar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import styles from "../../styles/TopNavBar.module.css";
+
+const TopNavBar = ({ currentPage }) => {
+    retu
