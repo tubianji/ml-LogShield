b7257c8f4b73beee3c3b7f37df65c60fe7fc2159
tubianji <130753460+tubianji@users.noreply.github.com>
2024-04-11 17:00:08 +0000

    remove outdated comments

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -32,4 +32,21 @@ module.exports = {
       // => @media (min-width: 768px) { ... }
 
       lg: "992px",
-      // => @media (min-width: 992px) { ... 
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1400px",
+      // => @media (min-width: 1400px) { ... }
+    },
+    extend: {
+      boxShadow: {
+        signUp: "0px 5px 10px rgba(4, 10, 34, 0.2)",
+        one: "0px 2px 3px rgba(7, 7, 77, 0.05)",
+        sticky: "inset 0 -1px 0 0 rgba(0, 0, 0, 0.1)",
+      },
+    },
+  },
+  plugins: [],
+};
